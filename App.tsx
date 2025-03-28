ff65690e208487ebed462cbe82a522773bfccb38
Joao Vitor Mendes Pascolat <ra2357062>
2025-03-28 12:02:51 -0300

    commit

--- App.tsx
+++ App.tsx
@@ -1,54 +1,107 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, StyleSheet, Text,TextInput, View } from 'react-native';
+import { Button, Dimensions, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+
+
 import _tarefa from "./types/_tarefas"
 import Tarefa from './components/Tarefa';
+import { Header } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   const [texto, setTexto] = useState<string>('')
   const [tarefas, setTarefas] = useState<_tarefa[]>([]);
-  function mostrarTarefas(){
-    return tarefas.map(t => <Tarefa key={t.id} dados={t} handleDeletePress={excluir}/>)
+  function mostrarTarefas() {
+    return tarefas.map(t => <Tarefa key={t.id} dados={t} handleDeletePress={excluir} />)
   }
-  function excluir (id:number){
-    let f = tarefas.filter (t =>t.id != id);
+  function excluir(id: number) {
+    let f = tarefas.filter(t => t.id != id);
     setTarefas(f);
   }
 
-  function adicionarTareda(){
-    if(texto==""){
+  function adicionarTareda() {
+    if (texto == "") {
       alert("insira um texto");
       return;
     }
-    let tarefa: _tarefa ={
-      id: tarefas.length +1,
-      texto:texto
+    let tarefa: _tarefa = {
+      id: tarefas.length + 1,
+      texto: texto
     };
 
-    setTarefas([...tarefas,tarefa]);
+    setTarefas([...tarefas, tarefa]);
   }
   return (
-    <View style={styles.container}>
-      <TextInput style={styles.input} value={texto} onChangeText={setTexto} />
-      <Button title="adicionar tarefa" onPress={adicionarTareda}/>
-      {mostrarTarefas()}
-    </View>
+  <SafeAreaProvider >
+      <SafeAreaView  style={styles.container} edges={['top']}>
+        <View style={styles.header}>To-do List</View>
+
+        <View style={styles.mid}>
+            <TextInput style={styles.input} value={texto} onChangeText={setTexto} />
+            <Button title="adicionar tarefa" onPress={adicionarTareda} />
+          </View>  
+          <View  style={styles.listaContainer}>
+            <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+              {mostrarTarefas()}
+            </ScrollView>
+          </View>
+        
+        <View style={styles.footer}> todos os direitos reservados ao &copy;João Vitor Mendes Pascolat</View>
+      </SafeAreaView >
+   </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  input:{
-    borderRadius:"10px",
-    color:"black",
-    padding:10,
-    margin:10,
-    borderColor:"black",
-    borderWidth:1,
+  mid:{
+    justifyContent:"center",
+    alignContent:"center",
+  },
+  listaContainer:{
+    justifyContent:"center",
+    alignItems:"center",
+    flex: 1,
+    width: "90%",
+  },
+  input: {
+    borderRadius: "10px",
+    color: "black",
+    padding: 10,
+    margin: 10,
+    borderColor: "black",
+    borderWidth: 1,
+  },
+  header: {
+    height: 50,
+    color: "white",
+    borderRadius: 10,
+    textAlign: "center",
+    width: "99%",
+    justifyContent: "center",
+    margin: 10,
+    backgroundColor: "#677E52",
+    fontFamily: "apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif",
+    fontSize: 20,
+
+  },
+  footer: {
+    height: 50,
+    color: "white",
+    borderRadius: 10,
+    textAlign: "center",
+    width: "99%",
+    justifyContent: "center",
+    margin: 10,
+    backgroundColor: "#677E52",
+    fontFamily: "apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif",
+    fontSize: 20,
   },
   container: {
+
     flex: 1,
-    backgroundColor: '#fff',
+
+    backgroundColor: '#E3D9D7',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "space-between",
   },
 });
--- components/Tarefa.tsx
+++ components/Tarefa.tsx
@@ -7,13 +7,29 @@ type tarefaProp = {
 }
 
 export default function Tarefa(props:tarefaProp){
-    return <View>
-     <Text>{props.dados.texto}</Text>
+    return <View style={styles.meio}>
+     <Text style={styles.texto}>{props.dados.texto}</Text>
         <Button color={"red"} title ="Excluir"
         onPress={()=> {props.handleDeletePress(props.dados.id)}}/>
     </View>;
 }
     const styles = StyleSheet.create({
+        meio:{
+            alignItems:"center",
+            width:230,
+        },
+        texto:{
+            fontFamily: "apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif",
+            width:230,
+            padding:10,
+            marginTop:10,
+            borderRadius:10,
+            textAlign:"center",
+            color:"white",
+            backgroundColor:"#677E52",
+            marginBottom:10,
+            fontSize:20,
+        },
         div:{
             borderWidth:1
         }
